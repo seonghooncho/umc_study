4b2347511881b22e567b210fe4e8b469720f26cd
CHO <imalex12@naver.com>
2024-10-26 22:11:22 +0900

    Add IntelliJ project to mission5 branch

---
+++ src/main/java/umc/study/domain/Region.java
@@ -0,0 +1,18 @@
+package umc.study.domain;
+import jakarta.persistence.*;
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Region extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
