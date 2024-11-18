fb9ed981b567fe7ec1691cea99b82e80a7bcd166
CHO <imalex12@naver.com>
2024-11-19 00:41:59 +0900

    refactor : 반환값 없는 성공케이스 추가
    resolve : #1

--- src/main/java/umc/study/apiPayload/ApiResponse.java
+++ src/main/java/umc/study/apiPayload/ApiResponse.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import umc.study.apiPayload.code.BaseCode;
 import umc.study.apiPayload.code.status.SuccessStatus;
 
+@Builder
 @Getter
 @AllArgsConstructor
 @JsonPropertyOrder({"isSuccess", "code", "message", "result"})
@@ -17,14 +19,26 @@ public class ApiResponse<T> {
     private final Boolean isSuccess;
     private final String code;
     private final String message;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("result")
     private T result;
 
 
     // 성공한 경우 응답 생성
 
     public static <T> ApiResponse<T> onSuccess(T result){
-        return new ApiResponse<>(true, SuccessStatus._OK.getCode() , SuccessStatus._OK.getMessage(), result);
+        return ApiResponse.<T>builder()
+                .isSuccess(true)
+                .code(SuccessStatus._OK.getCode())
+                .message(SuccessStatus._OK.getMessage())
+                .result(result)
+                .build();
+    }
+    public static <T> ApiResponse<T> onSuccess() {
+        return ApiResponse.<T>builder()
+                .isSuccess(true)
+                .code(SuccessStatus._OK.getCode())
+                .message(SuccessStatus._OK.getMessage())
+                .build();
     }
 
     public static <T> ApiResponse<T> of(BaseCode code, T result){
